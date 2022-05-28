bdc6da2dbe14cc5ba4906881543ca1a3250e877a
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-05-28 13:06:28 +0900

    一旦追加。

---
+++ Assets/Tsuguhiko/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField, Header("スコアテキスト")] Text _scoreText;
+
+    static int _score;
+
+    public static int Score => _score;
+
+
+    void Start()
+    {
+        _score = 0;
+        _scoreText.text = "Score: " + _score.ToString("D4");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
