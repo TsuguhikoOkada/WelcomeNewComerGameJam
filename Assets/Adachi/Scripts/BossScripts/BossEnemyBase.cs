86d4269b4bd21b96de4bc36966acd472c3365b20
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-05-28 12:54:27 +0900

    ボス制作中

---
+++ Assets/Adachi/Scripts/BossScripts/BossEnemyBase.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ボス敵の基底クラス
+/// </summary>
+public class BossEnemyBase : MonoBehaviour
+{
+    public Rigidbody2D Rb2D { get; set; }
+
+    /// <summary>ボスの体力のプロパティ</summary>
+    public int Hp { get; set; }
+
+    /// <summary>ボスの移動速度のプロパティ</summary>
+    public int Speed { get; set; }
+
+    public float Timer { get; set; }
+
+
+    Rigidbody2D _rb2D;
+
+    /// <summary>ボスの体力</summary>
+    [SerializeField, Header("体力")] int _hp;
+
+    /// <summary>ボスの移動速度</summary>
+    [SerializeField, Header("移動速度")] float _speed;
+
+    
+    float _timer;
+
+
+    protected virtual void Start()
+    {
+        Debug.Log("Start");
+        _rb2D = GetComponent<Rigidbody2D>();
+        RandomAction();
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+    }
+
+    protected virtual void RandomAction()
+    {
+        switch (Random.Range(0, 0))
+        {
+            case 0:
+                ActionPatten0();
+                break;
+            case 1:
+                ActionPatten1();
+                break;
+            case 2:
+                ActionPatten2();
+                break;
+            case 3:
+                ActionPatten3();
+                break;
+        }
+    }
+
+    protected virtual void ActionPatten0()
+    {
+
+    }
+
+    protected virtual void ActionPatten1()
+    {
+
+    }
+
+    protected virtual void ActionPatten2()
+    {
+
+    }
+
+    protected virtual void ActionPatten3()
+    {
+
+    }
+}
---
+++ Assets/Adachi/Scripts/BossScripts/BossMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    Rigidbody2D _rb2D;
+    /// <summary>水平、横方向</summary>
+    float _horizontal = 0f;
+    /// <summary>垂直、縦方向</summary>
+    float _veritical = 0f;
+    /// <summary>方向</summary>
+    Vector2 _dir;
+    /// <summary>移動スピード</summary>
+    [SerializeField, Header("移動スピード")] float _speed = 2f;
+    /// <summary>停止時間</summary>
+    [SerializeField, Header("停止時間")] float _stopTime = 2f;
+    /// <summary>移動時間</summary>
+    [SerializeField, Header("移動時間")] float _moveTime = 0.5f;
+    /// <summary>右限</summary>
+    [SerializeField, Header("右限")] float _rightLimit = 4f;
+    /// <summary>左限</summary>
+    [SerializeField, Header("左限")] float _leftLimit = -4f;
+    /// <summary>上限</summary>
+    [SerializeField, Header("上限")] float _upperLimit = 2.5f;
+    /// <summary>下限</summary>
+    [SerializeField, Header("下限")] float _lowerLimit = 1.5f;
+    /// <summary>左方向</summary>
+    const float LEFT_DIR = -1f;
+    /// <summary>右方向</summary>
+    const float RIGHT_DIR = 1f;
+    /// <summary>上方向</summary>
+    const float UP_DIR = 1f;
+    /// <summary>下方向</summary>
+    const float DOWN_DIR = -1f;
+
+    void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(RandomMovement());;
+    }
+
+
+    IEnumerator RandomMovement()
+    {
+        Debug.Log("OK");
+        while (true)
+        {
+            //一定時間止まる
+            _rb2D.velocity = Vector2.zero;
+            yield return new WaitForSeconds(_stopTime);
+
+            //場所によって移動できる左右方向を制限する
+            if (_rb2D.transform.position.x > _rightLimit)         //右に移動しすぎたら
+            {
+                _horizontal = Random.Range(LEFT_DIR, 0);//左移動可能
+            }
+            else if (_rb2D.transform.position.x < _leftLimit)   //左に移動しぎたら
+            {
+                _horizontal = Random.Range(0, RIGHT_DIR);//右移動可能
+            }
+            else　　　　　　　　　　　　         //左右どっちにも移動しすぎてないなら
+            {
+                _horizontal = Random.Range(LEFT_DIR, RIGHT_DIR);//自由に左右移動可能          
+            }
+
+            //場所によって移動できる上下方向を制限する
+            if (_rb2D.transform.position.y > _upperLimit)      //上に移動しすぎたら
+            {
+                _veritical = Random.Range(DOWN_DIR, 0);//下移動可能
+            }
+            else if (_rb2D.transform.position.y < _lowerLimit)//下に移動しすぎたら
+            {
+                _veritical = Random.Range(0, UP_DIR);//上移動可能
+            }
+            else　　　　　　　　　　　　　　      //上下どっちにも移動しすぎてないなら
+            {
+                _veritical = Random.Range(DOWN_DIR, UP_DIR);//自由に上下移動可能
+            }
+
+            _dir = new Vector2(_horizontal, _veritical);//ランダムに移動
+
+            //一定時間移動する
+            _rb2D.velocity = _dir.normalized * _speed;
+            yield return new WaitForSeconds(_moveTime);
+        }
+    }
+}
